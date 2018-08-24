fd775ed487af169b27a73f6a771a428588e7293b
Karsten Lang Pedersen <karstenlang@gmail.com>
2018-08-24 10:19:41 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="klang-globconf",
-    version="0.0.4",
+    version="0.0.5",
     author="Steffen Schumacher",
     author_email="ssch@wheel.dk",
     description="global configparser object to be used across modules",
